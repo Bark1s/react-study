110188545fc3950ae3242d7d075cf5a7ed32262f
Bark1s <sheda781997b@gmail.com>
2024-05-22 14:03:34 -0400

    h

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <Card>
+      <h1>Card title</h1>
+      <p>Text between opening and closing tag</p>
+    </Card>
+  );
+};
+
+export default App;
+import Card from "./Books";
